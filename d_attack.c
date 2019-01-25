964a88f85b3ef15d7c080955b722bc624f35f15f
PramodJose <pramod.gj17@gmail.com>
2019-01-25 21:03:29 +0530

    Code for thread creation and joining written.

--- d_attack.c
+++ d_attack.c
@@ -1,60 +1,110 @@
-//#include <pthreads.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define	MAX_THREADS	8
+#define LINE_LEN	64
 
 typedef enum {standard, given_in_class} dict_type;
-
-void debug(char* restrict dictionary, long beg, long end, int id);
-
-void d_attack(char* restrict dictionary, dict_type type)
+typedef struct
 {
-	FILE* dictionary_in = fopen(dictionary, "r");
-	long file_size, beg = 0, end = 0;
-	int i, pos_in_file;
+	char *dictionary, temp_file_name[8];
+	dict_type type;
+	long int begin, end;
+	FILE* out_fh;
+	int cracked_count;
+} thread_args;
 
-	fseek(dictionary_in, 0, SEEK_END);
-	file_size = ftell(dictionary_in);
 
-	for(i = 1; i <= MAX_THREADS; ++i)
+void* thread_entry(void*);
+
+
+void d_attack(char* restrict dictionary, char* restrict out, dict_type type)
+{
+	FILE *dictionary_fh = fopen(dictionary, "r"), *out_fh = fopen(out, "w"), *thread_in;
+	long file_size, fraction, beg = 0, end = 0;
+	int i;
+	pthread_t thread_id [MAX_THREADS];
+	thread_args args [MAX_THREADS];
+	char line [LINE_LEN];
+
+	fseek(dictionary_fh, 0, SEEK_END);
+	file_size = ftell(dictionary_fh);
+	fraction = file_size / MAX_THREADS;
+
+	for(i = 0; i < MAX_THREADS; ++i)
 	{
-		if(i != MAX_THREADS)
+		if(i != MAX_THREADS - 1)
 		{
-			pos_in_file = i * (file_size / MAX_THREADS);
-			fseek(dictionary_in, pos_in_file, SEEK_SET);
+			end = (i + 1) * fraction;
+			fseek(dictionary_fh, end, SEEK_SET);
 
-			while(fgetc(dictionary_in) != '\n');
+			while(fgetc(dictionary_fh) != '\n');
 
-			end = ftell(dictionary_in) - 1;
+			end = ftell(dictionary_fh) - 1;
 		}
 		else
 			end = file_size - 1;
 
 		//call to thread
-		debug(dictionary, beg, end, i);
+		args[i].dictionary = dictionary;
+		args[i].type = type;
+		args[i].begin = beg;
+		args[i].end = end;
+		strcpy(args[i].temp_file_name, "XXXXXX");
+		args[i].out_fh = fdopen(mkstemp(args[i].temp_file_name), "w+");
+
+		pthread_create(&thread_id[i], NULL, thread_entry, (void*) &args[i]);
+
 		beg = end + 1;
 	}
 
-	fclose(dictionary_in);
+	fclose(dictionary_fh);
+
+	// joining the threads.
+	for(i = 0; i < MAX_THREADS; ++i)
+	{
+		pthread_join(thread_id[i], NULL);
+
+		thread_in = args[i].out_fh;
+		fseek(thread_in, 0, SEEK_SET);
+
+		while(fgets(line, LINE_LEN, thread_in) != NULL)
+			fprintf(out_fh, "%s", line);
+
+		fclose(thread_in);
+		remove(args[i].temp_file_name);
+	}
+	fclose(out_fh);
 }
 
 
-void debug(char* restrict dictionary, long beg, long end, int id)
+void* thread_entry(void* arguments)
 {
-	FILE* fin = fopen(dictionary, "r");
-	char buffer[64];
-	int count = 0;
+	thread_args* args = arguments;
+	FILE* dictionary_fh = fopen(args->dictionary, "r");
+	long int end = args->end;
+	char d_line [LINE_LEN];
+	int i_dummy;
+	float f_dummy;
 
-	fseek(fin, beg, SEEK_SET);
+	args->cracked_count = 0;
+	fseek(dictionary_fh, args->begin, SEEK_SET);
 
-	printf("My ID is %d\n", id);
-	do
-	{
-		fgets(buffer, 64, fin);
-		printf("%s", buffer);
-		++count;
-	} while(ftell(fin) < end);
+	if(args->type == given_in_class)
+		do
+		{
+			fscanf(dictionary_fh, "%d%f%d%s", &i_dummy, &f_dummy, &i_dummy, d_line);
+			fprintf(args->out_fh, "%s\n", d_line);
+		} while(ftell(dictionary_fh) < end);
+	else
+		do
+		{
+			// Need to write code for general case (rockyou.txt).
+			break;
+		} while(ftell(dictionary_fh) < end);
 
-	printf("count: %d\n\n", count);
-	fprintf(stderr, "count: %d\n\n", count);
 
-	fclose(fin);
+	fclose(dictionary_fh);
+	return NULL;
 }
