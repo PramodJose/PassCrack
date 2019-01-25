52f0353b00d71067a29c306dff937876983b981e
PramodJose <pramod.gj17@gmail.com>
2019-01-26 03:34:18 +0530

    Code for basic dictionary attack done

--- d_attack.c
+++ d_attack.c
@@ -1,8 +1,9 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <string.h>
+#include <crypt.h>
 
-#define	MAX_THREADS	4
+#define	MAX_THREADS	8
 #define LINE_LEN	400
 
 typedef struct
@@ -52,6 +53,7 @@ void d_attack(char* restrict dictionary, char* restrict out, dict_type type)
 		args[i].end = end;
 		strcpy(args[i].temp_file_name, "XXXXXX");
 		args[i].out_fh = fdopen(mkstemp(args[i].temp_file_name), "w+");
+		printf("%d -> %s\n", i, args[i].temp_file_name);
 
 		pthread_create(&thread_id[i], NULL, thread_entry, (void*) &args[i]);
 
@@ -63,6 +65,7 @@ void d_attack(char* restrict dictionary, char* restrict out, dict_type type)
 	// joining the threads.
 	for(i = 0; i < MAX_THREADS; ++i)
 	{
+		printf("I\'m waiting\n");
 		pthread_join(thread_id[i], NULL);
 
 		thread_in = args[i].out_fh;
@@ -74,6 +77,7 @@ void d_attack(char* restrict dictionary, char* restrict out, dict_type type)
 		fclose(thread_in);
 		remove(args[i].temp_file_name);
 	}
+
 	fclose(out_fh);
 }
 
@@ -83,26 +87,36 @@ void* thread_entry(void* arguments)
 	thread_args* args = arguments;
 	FILE* dictionary_fh = fopen(args->dictionary, "r");
 	long int end = args->end;
-	char d_line [LINE_LEN];
-	int i_dummy;
+	char d_line [LINE_LEN], *calculated_hash;
+	int users_count, i, *users_list;
 	float f_dummy;
+	struct crypt_data data;
+	data.initialized = 0;
 
 	args->cracked_count = 0;
 	fseek(dictionary_fh, args->begin, SEEK_SET);
 
-	
 	do
 	{
 		if(args->type == given_in_class)
-			fscanf(dictionary_fh, "%d%f%d%s", &i_dummy, &f_dummy, &i_dummy, d_line);
+			fscanf(dictionary_fh, "%d%f%d%s", &users_count, &f_dummy, &users_count, d_line);
 		else
 		{
 			fgets(d_line, LINE_LEN, dictionary_fh);
-			i_dummy = strlen(d_line);
-			d_line[i_dummy - 1] = '\0';
+			users_count = strlen(d_line);
+			d_line[users_count - 1] = '\0';
 		}
 
-		fprintf(args->out_fh, "%s\n", d_line);
+		//fprintf(args->out_fh, "%s\n", d_line);
+		data.initialized = 0;
+		calculated_hash = crypt_r(d_line, "$1$GC$", &data);
+		users_list = retrieve_users(user_hashes, calculated_hash + 6, &users_count);
+		if(users_list != NULL)
+		{
+			for(i = 0 ; i < users_count; ++i)
+				fprintf(args->out_fh, "%d ", users_list[i]);
+			fprintf(args->out_fh, "%s\n\n", d_line);
+		}
 
 	} while(ftell(dictionary_fh) < end);
 
